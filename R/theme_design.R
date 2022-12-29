3ebccb5a601e37d807f2cd39b98815949203c453
Chris Martin <chrisjmartin03@gmail.com>
2022-12-29 14:44:29 +0000

    complete basic package set up (Chapter 2 of R packages book)

--- R/theme_design.R
+++ R/theme_design.R
@@ -1,3 +1,13 @@
-theme_design <- function(p){
-  return(p)
+#' theme_design
+#'
+#' @param arg1 theme argument 1
+#'
+#' @return a ggplot2 theme
+#' @export
+#'
+#' @examples
+#'
+#'
+theme_design <- function(arg1){
+  return(arg1)
 }
---
+++ tests/testthat/test-theme_design.R
@@ -0,0 +1,3 @@
+test_that("theme_design returns the argument passed to it", {
+  expect_equal(theme_design(2), 2)
+})
